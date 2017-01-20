448f2d45cb8e74523b459466279edcc32b3f8117
Dan <dan@noemail>
2017-01-20 13:43:09 -0500

    this works

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-# This assumes that the model has been mounted at /opt/tf-model
-/opt/tensorflow_serving/example/inception_export --checkpoint_dir=/opt/tf-model --export_dir=/opt/tf-export
+# This assumes that the exported model has been mounted at /opt/tf-export
 /opt/tensorflow_serving/model_servers/tensorflow_model_server --port=9000 --model_name=inception --model_base_path=/opt/tf-export
